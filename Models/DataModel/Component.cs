d50a400e736302bb7eac44a8821d8b2eaa4aed07
Frooxius <tomas.mariancik@gmail.com>
2025-12-31 14:44:03 -0800

    feat: Update component model to include type (and do not preallocate members)

--- Models/DataModel/Component.cs
+++ Models/DataModel/Component.cs
@@ -8,9 +8,17 @@ namespace ResoniteLink
     public class Component : Worker
     {
         /// <summary>
+        /// Datatype of the component, specified using Resonite's notation (equivalent to the C# notation)
+        /// This does not need to be specified when updating existing component, as the type cannot be
+        /// updated over components lifetime.
+        /// </summary>
+        [JsonPropertyName("componentType")]
+        public string ComponentType { get; set; }
+
+        /// <summary>
         /// Members (fields, references, lists...) of this component and their data
         /// </summary>
         [JsonPropertyName("members")]
-        public Dictionary<string, Member> Members { get; set; } = new Dictionary<string, Member>(); 
+        public Dictionary<string, Member> Members { get; set; }
     }
 }
