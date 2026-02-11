cf83b4886904beee603123af64eb5be4da32555a
Frooxius <tomas.mariancik@gmail.com>
2026-02-11 22:37:18 +0100

    tweak#: Add extra documentation

--- ResoniteLink/Models/Messages/DataModel/DataModelOperation.cs
+++ ResoniteLink/Models/Messages/DataModel/DataModelOperation.cs
@@ -5,6 +5,10 @@ using System.Text.Json.Serialization;
 
 namespace ResoniteLink
 {
+    /// <summary>
+    /// Represents messages that operate on the data model - either fetching information or modifying it.
+    /// These messages are synchronized with the update loop of the world as soon as possible before being applied.
+    /// </summary>
     public abstract class DataModelOperation : Message
     {
 
--- ResoniteLink/Models/Responses/BatchResponse.cs
+++ ResoniteLink/Models/Responses/BatchResponse.cs
@@ -5,6 +5,11 @@ using System.Text.Json.Serialization;
 
 namespace ResoniteLink
 {
+    /// <summary>
+    /// Represents a response to a batch of messages, e.g DataModelOperationBatch, containing individual responses to each.
+    /// IMPORTANT: Note that Success on this message itself only indicates that the batch itself was processed successfully,
+    /// but not necessarily that each individual message has succeeded. You need to check each individual response for this.
+    /// </summary>
     public class BatchResponse : Response
     {
         /// <summary>
